a3c65545f73cce2a42b5981d6d45a44651bcf6f4
Madhab Tripathy <madhabtripathy1349@gmail.com>
2023-03-19 17:38:53 +0530

    S1

--- src/main/java/com/driver/services/BlogService.java
+++ src/main/java/com/driver/services/BlogService.java
@@ -23,11 +23,27 @@ public class BlogService {
 
     public Blog createAndReturnBlog(Integer userId, String title, String content) {
         //create a blog at the current time
-
+        Blog blog = new Blog();
+        // set blog title and content
+        blog.setTitle(title);
+        blog.setContent(content);
+        // blog is created by the current userId
+        User user = userRepository1.findById(userId).get();
+        List<Blog> blogList = user.getBlogList();
+        blogList.add(blog);
+        // user set his blog list
+        user.setBlogList(blogList);
+        blog.setUser(user);
+        // save in database
+        blogRepository1.save(blog);
+        return blog;
     }
 
     public void deleteBlog(int blogId){
         //delete blog and corresponding images
-
+        Blog blog = blogRepository1.findById(blogId).get();
+        List<Image> imageList = blog.getImageList();
+        imageList.clear();
+        blogRepository1.deleteById(blogId);
     }
 }
--- src/main/java/com/driver/services/ImageService.java
+++ src/main/java/com/driver/services/ImageService.java
@@ -17,15 +17,26 @@ public class ImageService {
 
     public Image addImage(Integer blogId, String description, String dimensions){
         //add an image to the blog
+        Image image = new Image();
+        image.setDescription(description);
+        image.setDimension(dimensions);
 
+        Blog blog = blogRepository2.findById(blogId).get();
+        List<Image> imageList = blog.getImageList();
+        imageList.add(image);
+
+        blog.setImageList(imageList);
+        imageRepository2.save(image);
+
+        return image;
     }
 
     public void deleteImage(Integer id){
-
+        blogRepository2.deleteById(id);
     }
 
     public int countImagesInScreen(Integer id, String screenDimensions) {
         //Find the number of images of given dimensions that can fit in a screen having `screenDimensions`
-
+        return 0;
     }
 }
